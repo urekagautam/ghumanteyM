5392c2718f95416d69982095437e223ef57ed8d7
Ureka Gautam <urekagautam19@gmail.com>
2025-11-19 20:31:45 +0545

    build fixed

--- components/MapContainerComponent.jsx
+++ components/MapContainerComponent.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useRef } from "react"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import L from "leaflet"
 import "leaflet/dist/leaflet.css"
-import styles from "./MapComponent.module.css"
+import styles from "./MapContainerComponent.css"
 
 // Fix default marker icons
 delete L.Icon.Default.prototype._getIconUrl
